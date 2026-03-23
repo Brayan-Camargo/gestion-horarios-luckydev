a305fe3cee803ccc8fa29621a61e1e85661cdc47
Brayan-Camargo <brayancamargoramirez@gmail.com>
2026-03-23 15:38:59 -0600

    Estructura inicial del proyecto Gestion de Horarios

---
+++ src/main/java/com/gestionhorarios/luckydev/LuckydevApplication.java
@@ -0,0 +1,13 @@
+package com.gestionhorarios.luckydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckydevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckydevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionhorarios/luckydev/LuckydevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionhorarios.luckydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckydevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
